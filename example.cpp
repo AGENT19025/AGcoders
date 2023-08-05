f47e7ca0a45e0b01d11c27a975f14bb584ef6648
kalla vishwas uday kiran <kallavishwasudaykiran@gmail.com>
2023-08-05 20:43:52 +0530

    Add files via upload

---
+++ example.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int a,b,c;
+    cout<<"enter value of a & b:";
+    cin>>a>>b;
+    c=a+b;
+    cout<<"sum of a & b:"<<c;
+    return 0;     
+}
